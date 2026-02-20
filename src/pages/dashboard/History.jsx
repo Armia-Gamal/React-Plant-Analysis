f37a76cbcfbfbe07e0450dffb72fe98fec5ff5d7
Armia-Gamal <armia.gamalll@gmail.com>
2026-02-20 22:21:27 +0200

    dd

--- src/pages/dashboard/History.jsx
+++ src/pages/dashboard/History.jsx
@@ -3,8 +3,10 @@ import "./History.css";
 export default function History() {
   return (
     <>
-      <h2>History</h2>
-      <p>Previous analyses will appear here.</p>
+        <div style={{ padding: "166px" }}>
+            <h2>History</h2>
+            <p>Previous analyses will appear here.</p>
+        </div>
     </>
   );
 }
--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Profile.css";
 export default function Profile() {
   return (
-    <div style={{ padding: "40px" }}>
+    <div style={{ padding: "166px" }}>
       <h1>Profile Page</h1>
       <p>User profile settings here.</p>
     </div>
