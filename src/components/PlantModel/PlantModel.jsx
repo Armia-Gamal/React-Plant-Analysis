e46ca62350b1a42d6bbff2050f19a029c79600fd
Armia Gamal Fekry Fawzy <armia.gamalll@gmail.com>
2026-03-23 23:38:51 +0200

    replace model

--- src/components/PlantModel/PlantModel.jsx
+++ src/components/PlantModel/PlantModel.jsx
@@ -1,20 +1,21 @@
 import { useGLTF } from "@react-three/drei";
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { useRef, useEffect } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
 
 export default function PlantModel() {
-  const { scene } = useGLTF("/plant.glb");
+  const { scene } = useGLTF("/aglaonema_plant.glb");
   const ref = useRef();
+  const { invalidate } = useThree(); // 🔥 مهم
 
   useFrame((state) => {
-    if (ref.current) {
-      ref.current.rotation.y += 0.003;
+    if (!ref.current) return;
 
-      ref.current.rotation.x = -0.3;
+    ref.current.rotation.y += 0.003;
+    ref.current.rotation.x = -0.3;
+    ref.current.position.y = Math.sin(state.clock.elapsedTime) * 0.2;
 
-      ref.current.position.y = Math.sin(state.clock.elapsedTime) * 0.2;
-    }
+    invalidate(); // 🔥 يرندر بس وقت الحركة
   });
 
-  return <primitive ref={ref} object={scene} scale={1.5} />;
+  return <primitive ref={ref} object={scene} scale={3.5} />;
 }
